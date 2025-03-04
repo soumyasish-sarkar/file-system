bc5e04e1e60f5974e9303edec3a84b310ef65b3c
soumyasish-sarkar <ssoumyasish@gmail.com>
2025-03-05 01:20:35 +0530

    my_getattr() funtion implementation

--- filesystem.c
+++ filesystem.c
@@ -2,9 +2,25 @@
 #include <fuse.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <errno.h>
+
+static int my_getattr(const char *path, struct stat *stbuf) {
+    memset(stbuf, 0, sizeof(struct stat));
+
+    if (strcmp(path, "/") == 0) {  // Root directory
+        stbuf->st_mode = S_IFDIR | 0755;  // Directory with 755 permissions
+        stbuf->st_nlink = 2;
+    } else {
+        return -ENOENT;  // File not found
+    }
+
+    return 0;
+}
 
 static struct fuse_operations myfs_operations = {
-    //Function will be added here
+    .getattr = my_getattr,
 };
 
 int main(int argc, char *argv[]) {
