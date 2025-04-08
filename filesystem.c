111b8ad0a9b7ca4e868675d4fe38d2aa6f36ee01
soumyasish-sarkar <ssoumyasish@gmail.com>
2025-04-08 13:12:48 +0530

    Step 3 : Registering our file system inside the kernel

--- filesystem.c
+++ filesystem.c
@@ -5,11 +5,26 @@
 
 #define FILESYSTEM_NAME "file_system"  // file_system
 
+//Define the File System Type
+static struct file_system_type file_system_type ={
+    .owner = THIS_MODULE,       //Module safety (reference count)
+    .name = FILESYSTEM_NAME,
+   // .mount = simple_mount,      //Sets up superblock and VFS structures, entry point for setting up the in-memory file system structure.
+   // .kill_sb = simple_kill_sb,  //Cleans up on unmount
+};
 
 //File System Registration Function
 static int __init file_system_init(void){   //__init a special Macro, used only during initialization
-    printk(KERN_INFO "file_system: Initialization of File System\n");
-    printk(KERN_INFO "Inside file_syetem_init function i.e, Registering Function\n");
+    int ret;
+    printk(KERN_INFO "file_system: Registering file_system\n");
+
+    //register file_system in the kernel
+    ret = register_filesystem(&file_system_type);
+    if(ret !=0) {
+        printk(KERN_INFO "file_sytem: Failed to register file_system\n");
+        return ret;
+    }
+    printk(KERN_INFO "file_system: file_system successfully registered\n");
 
     return 0; //success
 }
@@ -17,8 +32,15 @@ static int __init file_system_init(void){   //__init a special Macro, used only 
 
 //File System Unregistration Function
 static void __exit file_system_exit(void){
-    printk(KERN_INFO "file_system : Unloading file system\n");
-    printk(KERN_INFO "Inside file_system_exit function i.e, Unregistering Function\n");
+    int ret;
+    printk(KERN_INFO "file_system: Unregistering file_system\n");
+
+    //unregister the file_system
+    ret = unregister_filesystem(&file_system_type);
+    if (ret != 0){
+        printk(KERN_INFO "file_system: Failed to unregister file_system\n");
+    }
+    printk(KERN_INFO "file_system: file_system successfully unregistered \n");
 }
 
 
