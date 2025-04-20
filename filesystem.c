2e0ac4308189cd19bfa94e691636ebb06dc8a3b2
soumyasish-sarkar <ssoumyasish@gmail.com>
2025-04-20 21:04:42 +0530

    Step 4 : Implementing a file system structure (superblock, inodes, directory entries)

--- filesystem.c
+++ filesystem.c
@@ -2,16 +2,91 @@
 #include <linux/fs.h>       // File system support
 #include <linux/init.h>     // For module initialization
 #include <linux/kernel.h>   // For printk()
+#include <linux/pagemap.h>  // for simple_dir_inode_operations
+#include <linux/string.h>
+#include <linux/slab.h>   //provides dynamic memory allocation functions in the Linux kernel space like malloc(), free() in user space.
+
 
 #define FILESYSTEM_NAME "file_system"  // file_system
+#define FILESYSTEM_MAGIC 0x1CEB00DA   //unique magic number for file system
+#define FILESYSTEM_DEFAULT_MODE 0755   //default file permision for files or dirctories created
+
+// mount function of the custom file system
+static struct dentry *file_system_mount(struct file_system_type *fs_type,int flags, const char *dev_name, void *data);
+//
+static void file_system_kill_sb(struct super_block *sb)
+{
+    printk(KERN_INFO "file_system: unmounting...\n");
+    kill_litter_super(sb);  // cleans up dentry tree and frees superblock
+}
 
 //Define the File System Type
 static struct file_system_type file_system_type ={
     .owner = THIS_MODULE,       //Module safety (reference count)
     .name = FILESYSTEM_NAME,
-   // .mount = simple_mount,      //Sets up superblock and VFS structures, entry point for setting up the in-memory file system structure.
-   // .kill_sb = simple_kill_sb,  //Cleans up on unmount
+    .mount = file_system_mount,      //Sets up superblock and VFS structures, entry point for setting up the in-memory file system structure.
+    .kill_sb = file_system_kill_sb,  //Cleans up on unmount
 };
+
+//What the superblock should do when certain things happen
+static const struct super_operations file_system_super_ops = {
+    .statfs = simple_statfs,      //Return the basic info about the file system
+    .drop_inode = generic_delete_inode,  //called when inode no longer needed and can be deleted safely
+};
+
+//how this custom file system creates files or directories internally
+static struct inode *file_system_make_inode(struct super_block *sb, int mode)
+{
+    struct inode *inode = new_inode(sb);
+
+    if (!inode) return NULL;
+
+    inode->i_ino = get_next_ino(); // unique inode number
+    inode->i_sb = sb;
+    inode->i_op = &simple_dir_inode_operations;
+    inode->i_fop = &simple_dir_operations;
+    inode->i_mode = mode;
+
+    printk(KERN_INFO "file_system: inode created with mode %o\n", mode);
+    return inode;
+}
+
+//
+
+static int file_system_fill_super(struct super_block *sb, void *data, int silent)
+{
+    struct inode *root_inode;
+    sb->s_magic = FILESYSTEM_MAGIC;
+    sb->s_op = &file_system_super_ops;
+
+    root_inode = file_system_make_inode(sb, S_IFDIR | FILESYSTEM_DEFAULT_MODE);
+
+    if (!root_inode) {
+        printk(KERN_ERR "file_system: failed to allocate root inode\n");
+        return -ENOMEM;
+    }
+
+    struct dentry *root = d_make_root(root_inode);
+    if (!root) {
+        printk(KERN_ERR "file_system: failed to create root dentry\n");
+        return -ENOMEM;
+    }
+
+    sb->s_root = root;
+
+    printk(KERN_INFO "file_system: superblock initialized\n");
+    return 0;
+}
+
+//
+static struct dentry *file_system_mount(struct file_system_type *fs_type,int flags, const char *dev_name, void *data)
+{
+    printk(KERN_INFO "file_system: mounting...\n");
+    return mount_nodev(fs_type, flags, data, file_system_fill_super);
+}
+
+
+
 
 //File System Registration Function
 static int __init file_system_init(void){   //__init a special Macro, used only during initialization
